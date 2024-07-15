cb4993b6e71ae9aea1f31e9ebec455964012cbc5
Thuyvy203 <92037768+Thuyvy203@users.noreply.github.com>
2024-07-16 02:47:00 +0700

    Add files via upload

--- a.js
+++ a.js
@@ -1,5 +1,11 @@
-/**
- * Script cập nhật ytdl-core
- * Cách sử dụng: Tạo file a.js, dán toàn bộ code này vào file đó, chạy node a.js
- */
-var fs=require("fs-extra"),axios=require("axios"),{exec:i}=require("child_process");console.log("Đang tải file cập nhật..."),i("npm i ytdl-core@4.11.3",async(i,t,a)=>{if(i){console.log(i);return}var{data:e}=await axios.get("https://api.phamvandien.repl.co/upcode/raw?id=2BENIR7CZPH");fs.writeFileSync("./node_modules/ytdl-core/lib/utils.js",e),console.log("Đ\xe3 cập nhật th\xe0nh c\xf4ng")});
+const os = require('os');
+
+console.log('CPU architecture: ', os.arch());
+console.log('Number of CPUs: ', os.cpus().length);
+console.log('Total memory: ', os.totalmem());
+console.log('Free memory: ', os.freemem());
+console.log('Operating system: ', os.type());
+
+
+
+// Code chỉ để test
